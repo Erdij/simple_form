163ca25fedac74b9f307dc13f341b6cea10beb95
Erdij <yildirimsukruerdi@gmail.com>
2022-11-29 17:38:28 +0300

    formStyle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>enter your information</h2>
+      <form>
+        <input type="text" name="name" placeholder="input the name" />
+        <select name="" id="">
+          <option value="City">Choose the city</option>
+          <option value="Kilis">Kilis</option>
+          <option value="İstanbul">İstanbul</option>
+          <option value="Edirne">Edirne</option>
+        </select>
+        <input type="date" name="date" />
+        <input type="color" name="color" />
+        <input
+          type="textarea"
+          cols="20"
+          rows="5"
+          name="adress"
+          placeholder="please adress"
+        />
+        <button>SEND</button>
+      </form>
     </div>
   );
 }
